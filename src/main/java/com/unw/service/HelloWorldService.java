8651c7846b7921e7463325ab162e736072e7cdae
Wolfgang Unger <wolfgang.unger@nttdata.com>
2018-05-22 15:11:57 +0200

    changes

--- src/main/java/com/unw/service/HelloWorldService.java
+++ src/main/java/com/unw/service/HelloWorldService.java
@@ -16,7 +16,7 @@ public class HelloWorldService {
     @GET
     @Path("/hello")
     public Response test() {
-        return Response.status(200).entity("Index Page + 22.05.2018").build();
+        return Response.status(200).entity("Index Page , Date 22.05.2018 updated").build();
     }
 
 }
