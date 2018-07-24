cf11de320fb585ff4a4f29a53d1487008f7ebdfe
Wolfgang Unger <wolfgang.unger@nttdata.com>
2018-07-24 11:00:26 +0200

    endpoint string

--- src/main/java/com/unw/service/HelloWorldService.java
+++ src/main/java/com/unw/service/HelloWorldService.java
@@ -16,7 +16,7 @@ public class HelloWorldService {
     @GET
     @Path("/hello")
     public Response test() {
-        return Response.status(200).entity("Index Page , Date 18.07.2018 - hello aws friends").build();
+        return Response.status(200).entity("Index Page , Date 24.07.2018 - hello aws friends - codebuild project").build();
     }
 
 }
