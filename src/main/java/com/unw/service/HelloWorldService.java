4d457ef338754521091d58d1800f8381993cbfd2
wolfgangunger <wolfgang.unger@gmx.de>
2019-05-10 09:18:52 +0200

    change

--- src/main/java/com/unw/service/HelloWorldService.java
+++ src/main/java/com/unw/service/HelloWorldService.java
@@ -16,7 +16,7 @@ public class HelloWorldService {
     @GET
     @Path("/hello")
     public Response test() {
-        return Response.status(200).entity("Index Page , Date 16.04.2019 - hello aws friends").build();
+        return Response.status(200).entity("Index Page , Date 10.05.2019 - hello aws meetup").build();
     }
 
 }
