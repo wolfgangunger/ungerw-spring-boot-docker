6691144bde62de73fd91dc51e15cfd9ad19c87cb
wolfgangunger <wolfgang.unger@gmx.de>
2019-05-17 18:29:16 +0200

    new version

--- src/main/java/com/unw/service/HelloWorldService.java
+++ src/main/java/com/unw/service/HelloWorldService.java
@@ -16,7 +16,7 @@ public class HelloWorldService {
     @GET
     @Path("/hello")
     public Response test() {
-        return Response.status(200).entity("Rest Enpoint , Date 10.05.2019 - HELLO AWS MEETUP - V1.1").build();
+        return Response.status(200).entity("Rest Enpoint , Date 10.05.2019 - HELLO AWS Friends - V1.1").build();
     }
 
 }
